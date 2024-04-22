36ebdc903323475482b76be1c9f18aed4c0c5bbc
IzzahKayani <167340588+IzzahKayani@users.noreply.github.com>
2024-04-22 11:09:40 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,17 +9,17 @@ using namespace std;
 int main()
 {
   string name;
-  string price;
-  string quantity;
+  int price;
+  int quantity;
 
-  ItemToPurchace item1;
-  ItemToPurchace item2;
+  ItemToPurchase item1;
+  ItemToPurchase item2;
   
   cout << "Item 1" << endl;
   cout << "Enter the item name:" << endl;
-  cin >> name;
+  getline(cin, name);
   item1.SetName(name);
-
+  
   cout << "Enter the item price:" << endl;
   cin >> price;
   item1.SetPrice(price);
@@ -27,26 +27,29 @@ int main()
   cout << "Enter the item quantity:" << endl;
   cin >> quantity;
   item1.SetQuantity(quantity);
+  cout << endl;
 
   cin.ignore();
-
+  
+  
   cout <<"Item 2" << endl;
 
   cout << "Enter the item name:" << endl;
-  cin >> name;
+  getline(cin, name);
   item2.SetName(name);
 
   cout << "Enter the item price:" << endl;
   cin >> price;
-  item1.SetPrice(price);
+  item2.SetPrice(price);
   
   cout << "Enter the item quantity:" << endl;
   cin >> quantity;
-  item1.SetQuantity(quantity);
+  item2.SetQuantity(quantity);
+  cout << endl;
   
-  cout << "TOTAL OUTPUT" << endl;
-  cout << item1.GetName() << " " << item1.GetQuantity() << " @ " << item1.GetPrice() << " = " << item1.GetQuantity()*item1.GetPrice() << endl;
-  cout << item2.GetName() << " " << item2.GetQuantity() << " @ " << item2.GetPrice() << " = " << item2.GetQuantity()*item2.GetPrice() << endl;
-  
+  cout << "TOTAL COST" << endl;
+  cout << item1.GetName() << " " << item1.GetQuantity() << " @ $" << item1.GetPrice() << " = $" << item1.GetQuantity()*item1.GetPrice() << endl;
+  cout << item2.GetName() << " " << item2.GetQuantity() << " @ $" << item2.GetPrice() << " = $" << item2.GetQuantity()*item2.GetPrice() << endl << endl;
+  cout << "Total: $" << item1.GetQuantity()*item1.GetPrice() + item2.GetQuantity()*item2.GetPrice() << endl;
   return 0;
 }
